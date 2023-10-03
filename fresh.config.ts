a09f308c19bb2a023366d9fc2101e9901b190be4
Oak Chantosa <o.chantosa@gmail.com>
2023-10-02 23:09:15 -0700

    feat: Cron job (#44)

--- fresh.config.ts
+++ fresh.config.ts
@@ -4,6 +4,14 @@ import twindConfig from "./twind.config.ts";
 import kvOAuthPlugin from "./plugins/kv_oauth.ts";
 import protectedRoutes from "./plugins/protected_routes.ts";
 import { FreshOptions } from "$fresh/server.ts";
+import { Cron } from "https://deno.land/x/croner@7.0.2/dist/croner.js";
+
+const EVERY_SUNDAY = "0 0 0 * * 7";
+
+// See https://deno.land/x/croner@7.0.2
+Cron(EVERY_SUNDAY, () => {
+  // TODO: run a job
+});
 
 export default {
   plugins: [kvOAuthPlugin, protectedRoutes, twindPlugin(twindConfig)],
